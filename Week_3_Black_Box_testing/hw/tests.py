85205de07de2f94f39e65869f8527d706acd63c1
Felix <111679444+mooseRobot@users.noreply.github.com>
2024-01-27 22:38:21 -1000

    Week 3 started

--- Week_3_Black_Box_testing/hw/tests.py
+++ Week_3_Black_Box_testing/hw/tests.py
@@ -11,7 +11,14 @@ class CreditCardTest(unittest.TestCase):
         self.assertTrue(credit_card_validator(4000000000000002))
 
     def test2(self):
+        """
+        Verify if credit_card_validator accurately identifies invalid cc prefixes.
+        """
         self.assertFalse(credit_card_validator(3000000000000002))
+        
+    def test3(self):
+        self.assertTrue(credit_card_validator('340000f00000000'))
+        
 
 
 if __name__ == '__main__':
