363f562ab315a9dae2a0237afd1199875d83eb85
Felix <111679444+mooseRobot@users.noreply.github.com>
2024-01-27 16:57:54 -1000

    uh

---
+++ Week_3_Black_Box_testing/hw/tests.py
@@ -0,0 +1,20 @@
+import unittest
+from credit_card_validator import credit_card_validator
+
+
+class CreditCardTest(unittest.TestCase):
+    def test1(self):
+        """
+        Verifies if check sum is correct
+        """
+        print(credit_card_validator(4000000000000002))
+        self.assertTrue(credit_card_validator(4000000000000002))
+
+    def test2(self):
+        self.assertFalse(credit_card_validator(3000000000000002))
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
